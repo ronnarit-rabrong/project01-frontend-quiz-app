c940b53fbd282827dda4ae0edb894599cebe7dd8
ronnarit-rabrong <ronnaritrabrong@gmail.com>
2026-06-05 00:03:02 +0700

    edit base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/project01-frontend-quiz-app/",
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
   ],
+  base: "/",
 })
